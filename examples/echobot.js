1411e693d5df25cae120d59080b77fdac18f2be0
Hydrant <zhangqiuyun@infohold.com.cn>
2019-02-12 15:38:49 +0800

    修改连接的openfire服务为外网地址

--- examples/echobot.js
+++ examples/echobot.js
@@ -1,4 +1,4 @@
-var BOSH_SERVICE = 'http://192.168.10.142:7070/http-bind/';//'/xmpp-httpbind';
+var BOSH_SERVICE = 'http://webim.cafewallet.cn/http-bind/';//'/xmpp-httpbind';
 var connection = null;
 
 function log(msg) 
